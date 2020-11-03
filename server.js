8015e5a96a43fb190220ff64d720e5815566c811
Melanie Hall <hallmt313@gmail.com>
2020-11-03 10:08:19 -0500

    added server.js inital set up

---
+++ server.js
@@ -0,0 +1,17 @@
+//Dependencies
+var express = require("express");
+
+//Express server set up
+var app = express();
+var PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Router - server map to respond to users request/visits
+require("./routes/route1")(app);
+
+//Listener to start the server
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
