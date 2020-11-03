3d5418be48858927d68a3e4b7b346c7ab43261ea
Melanie Hall <hallmt313@gmail.com>
2020-11-03 12:23:12 -0500

    more on api routing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,32 @@
 //Load Data
+var notesData = require("../db/db.json");
+//Dependencies
+var fs = require("fs");
+var express = require('express');
 
-var notesData = require("../data/notes");
+// express server set up? do we need this here and in the server.js?
+var app = express();
 
 //Routing
 module.exports = function(app){
     //api get request
     app.get("api/notes", function(req, res){
-        //res.json(notesData);
+        res.json(notesData);
     });
 
     // api post request
     app.post("/api/notes", function(req, res){
-        notesData.push(req);
-        //res.json(true);
+        var newNote = req.body;
+        //need to add data to the db.json
+        fs.appendFile('db.json', JSON.stringify(newNote), function(err){
+            if (err) {
+                return console.log(err);
+              }
+        });
     });
 
     //api delete request
-    app.delete("api/notes/", function(req, res){
-        
-    });
+    // app.delete("api/notes/", function(req, res){
+
+    // });
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app){
         res.sendFile(path.join(__dirname, "../public/notes.html"));
       });
     //Get request for any other route - aka home/index
-    app.get("*", function(req, res) {
+    app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 }
